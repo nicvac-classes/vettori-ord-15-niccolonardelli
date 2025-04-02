1c4d4438b45dad4623d2ea6fe33c9086804c31b0
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2025-04-02 15:24:36 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
 using namespace std;
-
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    srand(time(0));
+    int i, n, j, t;
+    string s;
+    bool scambio;
+    cout << "quante macchine hanno gareggiato?" << endl;
+    cin >> n;
+    vector<int>r(n);
+    vector<string>nomi(n);
+    i=0;
+    while (i<n)
+    {
+        cout << "come si chiama la macchina?" << endl;
+        cin >> nomi[i];
+        r[i] = rand()%(n+1);
+        i=i+1;
+    }
+    i=0;
+    scambio = true;
+    while (i<n and scambio)
+    {
+        j=0;
+        scambio= false;
+        while (j<=n-2-i)
+        {
+            if (r[j]>r[j+1])
+            {
+                t=r[j];
+                r[j]= r[j+1];
+                r[j+1] = t;
+                s=nomi[j];
+                nomi[j]=nomi[j+1];
+                nomi[j+1] =s;
+                scambio=true;
+            }
+            j=j+1;
+        }
+        i=i+1;
+    }
+    j=0;
+    while(j<n)
+    {
+        cout << "l'auto " << nomi[j] << " è arrivata nella posizione numero " << r[j] << endl;
+        j=j+1;
+    }
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
